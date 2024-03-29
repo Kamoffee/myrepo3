6ecd7b9188b06c5ba44f996c9b15ed214fc33918
Andrei Miron <victormiron.home@gmail.com>
2024-03-29 13:23:05 -0400

    This finally worked on code ocean

--- encrypted_sentence.py
+++ encrypted_sentence.py
@@ -1,26 +1,42 @@
-# number_to_shift = int(input("Please enter a number of places to shift:"))
-# text = input("Please enter a sentence: ")
-# alpha = "abcdefghijklmnopqrstuvwxyz"
-# new_alpha = list(alpha)
-# text_low = text.lower()
-# new_list = []
+# Define a function
+def caesar (number_to_shift, text_low): 
+    # Checking to see if the user input is decimal and in range
+    if not number_to_shift.isdecimal() or not (0 <= int(number_to_shift) <= 25) :
+        print("You need to enter a number between 0 and 25!") 
+    else:
+        # set our variables
+        number_to_shift = int(number_to_shift)
+        alpha = "abcdefghijklmnopqrstuvwxyz"
+        new_alpha = list(alpha)
+        new_list = []
 
-# for letters in text_low:
-#     # Check if the letters are in the alphabet
-#     if letters in alpha:
-#         index = alpha.index(letters)
-#     # Apply the shift of input
-#         new_index = (index + number_to_shift) %  26
-#         new_letters = new_alpha[new_index]
-#         new_list.append(new_letters)
-#     else:
-#         # keep non - characters from the sentence
-#         new_list.append(letters)
+        for letters in text_low.lower():
+            # Check if the letters are in the alphabet
+            if letters in alpha:
+                index = alpha.find(letters.lower())
+            # Apply the shift of input
+                new_index = (index + number_to_shift) %  26
+                new_letters = new_alpha[new_index]
+                new_list.append(new_letters)
+            else:
+                # keep non - characters from the sentence
+                new_list.append(letters)
 
-# new_text = ''.join(new_list)
-# print(new_text)
+        new_text = ''.join(new_list)
+        return new_text
 
-## another aproach by what open sap wanted before
+# Creating a main function to coordinate caesar function
+def main():
+    # Asking for user input in our caesar variables
+    number_to_shift = input("Please enter a number of places to shift:") 
+    text_low = input("Please enter a sentence: ")
+    cypher = caesar(number_to_shift, text_low)
+    print(cypher)
+
+
+main()
+
+## another aproach 
 
 # # asking for user input
 # number_to_shift = input("Please enter the number of places to shift: ")
@@ -37,6 +53,7 @@
 # if not 0 <= number_to_shift <= 25:
 #     print("error")
 #     exit()
+
 
 # text_low = text.lower()
 # alpha = "abcdefghijklmnopqrstuvwxyz"
@@ -62,38 +79,4 @@
 # # print the encrypted text
 # print("Encrypted text:", encrypted_text)
 
-### what opensap wants with error
-number_to_shift = input("Please enter the number of places to shift: ")
-# check if input number is decimal.
-if not number_to_shift.isdecimal():
-    print("Error")
-else:
-    number_to_shift = int(number_to_shift) % 26
-    # check that the user input a number between 1 and 25
-    if not 0 <= number_to_shift <= 25:
-        print("Error: The shift value must be between 0 and 25.")
 
-
-text = input("Please enter a sentence: ")
-text_low = text.lower()
-alpha = "abcdefghijklmnopqrstuvwxyz"
-new_text = ""
-
-for letter in text_low:
-    if letter.isalpha():
-        index = alpha.find(letter.lower())
-        if index != -1:
-            new_index = (index + number_to_shift) % 26  # Applying the shift of input
-            new_letter = alpha[new_index]
-        # Preserve the case of the original letter
-            if letter.isupper():
-                new_letter = new_letter.upper()
-            new_text += new_letter
-        else:
-        # keep non -alphabetic characters unchanged
-            new_text += letter
-    else:
-        new_text += letter
-
-
-print("The encrypted sentence is:", new_text)
